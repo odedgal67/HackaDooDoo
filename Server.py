5c97b1a59fb3706fec41e525748f7a025a996ece
sashajke <uzelevsk@post.bgu.ac.il>
2021-12-28 21:51:43 +0200

    added try cstch to server

--- Server.py
+++ Server.py
@@ -45,7 +45,7 @@ def offerStage():
             sleep(1)
         udpSocket.close()
     except Exception as e:
-        print(e)
+        print("From offerStage" + e)   
         udpSocket.close()    
 
 
@@ -57,51 +57,58 @@ def read_name(conn):
         print("{} has joined the game".format(clientName))
         return clientName
     except Exception as e:
-        print(e)
+        print("From read_name" + e)   
         conn.close()
 
 
 def handle_client(conn, clientIndex, question, answer):
     global winningTeam
     msg = "Welcome to Quick Maths. \nPlayer 1: {} \nPlayer 2: {} \n== \nPlease answer the following question as fast as you can: \nHow much is {}?".format(clientNames[0], clientNames[1],question)
+    try:
+        conn.send(msg.encode())
+        clientAns = conn.recv(MAX_BUFFER_SIZE).decode()
 
-    conn.send(msg.encode())
-    clientAns = conn.recv(MAX_BUFFER_SIZE).decode()
-
-    if(clientAns == answer and clientAns != ""):  # correct answer
-        handleClientLock.acquire()
-        if winningTeam == -1:  # if no one has answered yet
-            winningTeam = clientIndex
-            sendGameSummary()
-            closeConnections()
-        handleClientLock.release()
-    elif (clientAns != ""):
-        handleClientLock.acquire()
-        if winningTeam == -1:
-            winningTeam = 1-clientIndex
-            sendGameSummary()
-            closeConnections()
-        handleClientLock.release()
-    # else:
-    #     sendGameSummary()
+        if(clientAns == answer and clientAns != ""):  # correct answer
+            handleClientLock.acquire()
+            if winningTeam == -1:  # if no one has answered yet
+                winningTeam = clientIndex
+                sendGameSummary()
+                closeConnections()
+            handleClientLock.release()
+        elif (clientAns != ""):
+            handleClientLock.acquire()
+            if winningTeam == -1:
+                winningTeam = 1-clientIndex
+                sendGameSummary()
+                closeConnections()
+            handleClientLock.release()
+    except Exception as e:
+        print("From handle_client" + e)   
+        conn.close()
 
 
 def accept_clients(serverSocket):
-    clientSocket1, addr1 = serverSocket.accept()
-    read_name(clientSocket1)
-    clientSocket2, addr2 = serverSocket.accept()
-    read_name(clientSocket2)
-    clientSockets.append(clientSocket1)
-    clientSockets.append(clientSocket2)
+    try:
+        clientSocket1, addr1 = serverSocket.accept()
+        read_name(clientSocket1)
+        clientSocket2, addr2 = serverSocket.accept()
+        read_name(clientSocket2)
+        clientSockets.append(clientSocket1)
+        clientSockets.append(clientSocket2)
+    except Exception as e:
+        print("From accept_clients" + e)    
 
 
 def start_server():
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serverSocket.bind((SERVER_IP, SERVER_PORT))
-    serverSocket.listen()  # server is listening for client connection
-    print("Server started, listening on IP address {}".format(SERVER_IP))
-    return serverSocket
+    try:
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serverSocket.bind((SERVER_IP, SERVER_PORT))
+        serverSocket.listen()  # server is listening for client connection
+        print("Server started, listening on IP address {}".format(SERVER_IP))
+        return serverSocket
+    except Exception as e:
+        print("From start_server" + e)       
 
 
 '''
@@ -114,20 +121,23 @@ def generateRandomQuestion() -> tuple:
 
 
 def sendGameSummary():
-    msg = ""
-    if winningTeam != -1:
-        # generate summary string
-        msg = "Game Over!\nThe correct answer was {}!\n\nCongratulations to the winner: {}".format(
-            currAnswer, clientNames[winningTeam]).encode()
-    else:
-        msg = "Game Over!\nThe correct answer was {}!\n\nThe game ended with a tie".format(
-            currAnswer).encode()
+    try:
+        msg = ""
+        if winningTeam != -1:
+            # generate summary string
+            msg = "Game Over!\nThe correct answer was {}!\n\nCongratulations to the winner: {}".format(
+                currAnswer, clientNames[winningTeam]).encode()
+        else:
+            msg = "Game Over!\nThe correct answer was {}!\n\nThe game ended with a tie".format(
+                currAnswer).encode()
 
-        # send summary to client
-    clientSoc = clientSockets[0]
-    clientSoc.send(msg)
-    clientSoc = clientSockets[1]
-    clientSoc.send(msg)
+            # send summary to client
+        clientSoc = clientSockets[0]
+        clientSoc.send(msg)
+        clientSoc = clientSockets[1]
+        clientSoc.send(msg)
+    except Exception as e:
+        print("From sendGameSummary" + e)
 
         
 
@@ -137,28 +147,30 @@ def printGameOver():
 
 
 def playGame():
-    global currQuestion, currAnswer
-    currQuestion, currAnswer = generateRandomQuestion()
-    client1GameThread = threading.Thread(
-        target=handle_client, args=(clientSockets[0], 0, currQuestion, currAnswer))
-    client2GameThread = threading.Thread(
-        target=handle_client, args=(clientSockets[1], 1, currQuestion,  currAnswer))
-    client1GameThread.start()
-    client2GameThread.start()
-    client1GameThread.join(timeout=4.0)
-    client2GameThread.join(timeout=4.0)
-    if (client1GameThread.is_alive() and client2GameThread.is_alive()):
-        sendGameSummary()
-
-    # send summary to both clients
-    # sendGameSummary()
-
-    printGameOver()
+    try:
+        global currQuestion, currAnswer
+        currQuestion, currAnswer = generateRandomQuestion()
+        client1GameThread = threading.Thread(
+            target=handle_client, args=(clientSockets[0], 0, currQuestion, currAnswer))
+        client2GameThread = threading.Thread(
+            target=handle_client, args=(clientSockets[1], 1, currQuestion,  currAnswer))
+        client1GameThread.start()
+        client2GameThread.start()
+        client1GameThread.join(timeout=4.0)
+        client2GameThread.join(timeout=4.0)
+        if (client1GameThread.is_alive() and client2GameThread.is_alive()):
+            sendGameSummary()
+        printGameOver()
+    except Exception as e:
+        print("From playGame" + e)       
 
 
 def closeConnections():
-    clientSockets[0].close()
-    clientSockets[1].close()
+    try:
+        clientSockets[0].close()
+        clientSockets[1].close()
+    except Exception as e:
+        print("From closeConnections" + e)       
 
 def resetGlobalVars():
     global winningTeam,needToOffer,clientSockets,clientNames
@@ -172,6 +184,7 @@ def Main():
     #     SERVER_IP = scapy.all.get_if_addr('eth1')
     # else:
     #     SERVER_IP = scapy.all.get_if_addr('eth2')
+
     serverSocket = start_server()
     while True:
         global needToOffer
@@ -185,17 +198,7 @@ def Main():
         resetGlobalVars()
     # closeConnections()
 
-
-# def accept_clients(server):
-#     while True:
-#         client, client_address = server.accept()
-
-
-# def send_offer_message:
-
-
-# start_server()
-# while True:
+    #TODO: sigint and excpetion check
 
 
 if __name__ == '__main__':

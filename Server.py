688a0c21ed739a67f9635c5e02e22b6d5cbf7fec
odedgal67 <62900689+odedgal67@users.noreply.github.com>
2021-12-29 18:42:54 +0200

    Ready for testing

--- Server.py
+++ Server.py
@@ -8,7 +8,9 @@ import random
 import scapy.all
 import sys
 
-#Colors 
+# Colors
+
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -21,15 +23,15 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-
-#Questions and answers bank
+# Questions and answers bank
 questionsList = [("2+2", "4"), ("5-2", "3"), ("9-7", "2"), ("8+1", "9"),
                  ("6-5", "1"), ("8-9+2", "1"), ("5+3", "8"), ("3+4", "7"),
-                 ("6+4-5", "5"), ("2+6-7+1","2"), ("(5*2)-3","7"), ("((5*10)/2)-19", "6")
-                 ("log(8)*((9/3)/3)","3")]
+                 ("6+4-5", "5"), ("2+6-7+1", "2"), ("(5*2)-3",
+                                                    "7"), ("((5*10)/2)-19", "6")
+                 ("log(8)*((9/3)/3)", "3")]
 
-#Global variables                 
-def_tstp_handler = None                 
+# Global variables
+def_tstp_handler = None
 currQuestion = None
 currAnswer = None
 SERVER_IP = socket.gethostbyname(socket.gethostname())
@@ -50,7 +52,7 @@ clientNames = []
 handleClientLock = threading.Lock()
 
 
-#-----------------------------------------------------functions--------------------------------------------------------------
+# -----------------------------------------------------functions--------------------------------------------------------------
 
 """
     Description:
@@ -60,6 +62,8 @@ handleClientLock = threading.Lock()
     Returns:
         void
 """
+
+
 def offerStage():
     try:
         udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -69,8 +73,9 @@ def offerStage():
             sleep(1)
         udpSocket.close()
     except Exception as e:
-        print("{}From offerStage {}{}".format(bcolors.FAIL,e,bcolors.ENDC))
+        print("{}From offerStage {}{}".format(bcolors.FAIL, e, bcolors.ENDC))
         udpSocket.close()
+
 
 """
     Description:
@@ -80,15 +85,18 @@ def offerStage():
     Returns:
         void
 """
+
+
 def read_name(conn):
     try:
         # recv is returning the name with \n at the end
         clientName = conn.recv(MAX_BUFFER_SIZE).decode()
         clientName = clientName[: -1]  # removing \n in the end of the name
         clientNames.append(clientName)
-        print("{}{} has joined the game{}".format(bcolors.OKGREEN,clientName,bcolors.ENDC))
+        print("{}{} has joined the game{}".format(
+            bcolors.OKGREEN, clientName, bcolors.ENDC))
     except Exception as e:
-        print("{}From read_name {}{}".format(bcolors.FAIL, e,bcolors.ENDC))
+        print("{}From read_name {}{}".format(bcolors.FAIL, e, bcolors.ENDC))
         conn.close()
 
 
@@ -100,6 +108,8 @@ def read_name(conn):
     Returns:
         void
 """
+
+
 def handle_client(conn, clientIndex, question, answer):
     global winningTeam
     msg = "Welcome to Quick Maths. \nPlayer 1: {} \nPlayer 2: {} \n== \nPlease answer the following question as fast as you can: \nHow much is {}?".format(
@@ -123,8 +133,10 @@ def handle_client(conn, clientIndex, question, answer):
                 closeConnections()
             handleClientLock.release()
     except Exception as e:
-        print("{}From handle_client {}{}".format(bcolors.FAIL,e,bcolors.ENDC))
+        print("{}From handle_client {}{}".format(
+            bcolors.FAIL, e, bcolors.ENDC))
         conn.close()
+
 
 """
     Description:
@@ -134,6 +146,8 @@ def handle_client(conn, clientIndex, question, answer):
     Returns:
         void
 """
+
+
 def accept_clients(serverSocket):
     try:
         clientSocket1, addr1 = serverSocket.accept()
@@ -143,7 +157,9 @@ def accept_clients(serverSocket):
         clientSockets.append(clientSocket1)
         clientSockets.append(clientSocket2)
     except Exception as e:
-        print("{}From accept_clients {}{}".format(bcolors.FAIL,e,bcolors.ENDC))
+        print("{}From accept_clients {}{}".format(
+            bcolors.FAIL, e, bcolors.ENDC))
+
 
 """
     Description:
@@ -153,16 +169,20 @@ def accept_clients(serverSocket):
     Returns:
         void
 """
+
+
 def start_server():
     try:
         serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         serverSocket.bind((SERVER_IP, SERVER_PORT))
         serverSocket.listen(2)  # server is listening for client connection
-        print("{}Server started, listening on IP address {}{}".format(bcolors.OKCYAN,SERVER_IP,bcolors.ENDC))
+        print("{}Server started, listening on IP address {}{}".format(
+            bcolors.OKCYAN, SERVER_IP, bcolors.ENDC))
         return serverSocket
     except Exception as e:
-        print("{}From start_server {}{}".format(bcolors.FAIL,e,bcolors.ENDC))
+        print("{}From start_server {}{}".format(bcolors.FAIL, e, bcolors.ENDC))
+
 
 """
     Description:
@@ -172,8 +192,11 @@ def start_server():
     Returns:
         tuple(String,int)
 """
+
+
 def generateRandomQuestion() -> tuple:
     return random.choice(questionsList)
+
 
 """
     Description:
@@ -183,16 +206,18 @@ def generateRandomQuestion() -> tuple:
     Returns:
         void
 """
+
+
 def sendGameSummary():
     try:
         msg = ""
         if winningTeam != -1:
             # generate summary string
             msg = "Game Over!\nThe correct answer was {}!\n\nCongratulations to the winner: {}{}".format(
-                currAnswer, clientNames[winningTeam],bcolors.ENDC).encode()
+                currAnswer, clientNames[winningTeam], bcolors.ENDC).encode()
         else:
             msg = "Game Over!\nThe correct answer was {}!\n\nThe game ended with a tie{}".format(
-                currAnswer,bcolors.ENDC).encode()
+                currAnswer, bcolors.ENDC).encode()
 
             # send summary to client
         clientSoc = clientSockets[0]
@@ -200,7 +225,9 @@ def sendGameSummary():
         clientSoc = clientSockets[1]
         clientSoc.send(msg)
     except Exception as e:
-        print("{}From sendGameSummary {}{}".format(bcolors.FAIL,e,bcolors.ENDC))
+        print("{}From sendGameSummary {}{}".format(
+            bcolors.FAIL, e, bcolors.ENDC))
+
 
 """
     Description:
@@ -210,8 +237,12 @@ def sendGameSummary():
     Returns:
         void
 """
+
+
 def printGameOver():
-    print("{}Game over, sending out offer requests...{}".format(bcolors.OKBLUE,bcolors.ENDC))
+    print("{}Game over, sending out offer requests...{}".format(
+        bcolors.OKBLUE, bcolors.ENDC))
+
 
 """
     Description:
@@ -221,6 +252,8 @@ def printGameOver():
     Returns:
         void
 """
+
+
 def playGame():
     try:
         global currQuestion, currAnswer
@@ -237,7 +270,8 @@ def playGame():
             sendGameSummary()
         printGameOver()
     except Exception as e:
-        print("{}From playGame {}{}".format(bcolors.FAIL,e,bcolors.ENDC))
+        print("{}From playGame {}{}".format(bcolors.FAIL, e, bcolors.ENDC))
+
 
 """
     Description:
@@ -247,16 +281,20 @@ def playGame():
     Returns:
         void
 """
+
+
 def closeConnections():
     try:
-        if(len(clientSockets)==2):
+        if(len(clientSockets) == 2):
             clientSockets[0].close()
             clientSockets[1].close()
         elif (len(clientSockets) == 1):
             clientSockets[0].close()
-            
+
     except Exception as e:
-        print("{}From closeConnections {}{}".format(bcolors.FAIL,e,bcolors.ENDC))
+        print("{}From closeConnections {}{}".format(
+            bcolors.FAIL, e, bcolors.ENDC))
+
 
 """
     Description:
@@ -266,12 +304,16 @@ def closeConnections():
     Returns:
         void
 """
+
+
 def resetGlobalVars():
     global winningTeam, needToOffer, clientSockets, clientNames
     winningTeam = -1
     needToOffer = True
     clientSockets = []
     clientNames = []
+
+
 """
     Description:
         this function is used to redefine a signal behaviour
@@ -280,13 +322,17 @@ def resetGlobalVars():
     Returns:
         void
 """
-def signal_handler(sig,frame):
+
+
+def signal_handler(sig, frame):
     print("\nServer closing peacfully...")
     closeConnections()
-    serverSocket.close() #close that server socket and free the port
-    if(sig==signal.SIGTSTP):
+    serverSocket.close()  # close that server socket and free the port
+    if(sig == signal.SIGTSTP):
         signal.signal(sig, def_tstp_handler)
         signal.raise_signal(sig)
+
+
 """
     Description:
         setting the global variables to initial values
@@ -295,37 +341,39 @@ def signal_handler(sig,frame):
     Returns:
         void
 """
-def setGlobals():
-    global def_tstp_handler,SERVER_PORT, udpMsg
 
-    #signals
-    def_tstp_handler =  signal.getsignal(signal.SIGTSTP)
+
+def setGlobals():
+    global def_tstp_handler, SERVER_PORT, udpMsg, SERVER_IP
+
+    # signals
+    def_tstp_handler = signal.getsignal(signal.SIGTSTP)
     signal.signal(signal.SIGTSTP, signal_handler)
 
-    #server port
+    # server port
     SERVER_PORT = random.randrange(5000, 7000)
 
-    #creating udp offer message to send
-    udpMsg =  struct.pack('IbH', MAGIC_COOKIE, MESSAGE_TYPE,
-                     SERVER_PORT)  # encoding udp message
+    # creating udp offer message to send
+    udpMsg = struct.pack('IbH', MAGIC_COOKIE, MESSAGE_TYPE,
+                         SERVER_PORT)  # encoding udp message
 
-    #set ip from user input
-    # print(Fore.Blue + "please select network type: \n1) dev network \n2)test network")
-    # typeOfNet = sys.stdin.readline()
-    # if typeOfNet == "1":
-    #     SERVER_IP = scapy.all.get_if_addr('eth1')
-    # else:
-    #     SERVER_IP = scapy.all.get_if_addr('eth2')
-    
+    # set ip from user input
+    print("please select network type: \n1) dev network \n2)test network")
+    typeOfNet = sys.stdin.readline()
+    if typeOfNet == "1":
+        SERVER_IP = scapy.all.get_if_addr('eth1')
+    else:
+        SERVER_IP = scapy.all.get_if_addr('eth2')
+
 
 def Main():
-    
+
     global serverSocket
-    
-    setGlobals() #set global variables to default values
-    serverSocket = start_server() 
-    
-    while serverSocket!=None: #infinite loop to keep server alive
+
+    setGlobals()  # set global variables to default values
+    serverSocket = start_server()
+
+    while serverSocket != None:  # infinite loop to keep server alive
         global needToOffer
         offer_thread = threading.Thread(target=offerStage)
         offer_thread.start()
@@ -335,7 +383,6 @@ def Main():
         sleep(10)
         playGame()
         resetGlobalVars()
-
 
 
 if __name__ == '__main__':
